73dc6da0222731b85e45f6263f0b3a7759d88562
guillaume <guillaume.laurent13880@gmail.com>
2023-04-16 15:35:04 +0200

    installation knp_paginator

--- src/Controller/PlatsController.php
+++ src/Controller/PlatsController.php
@@ -3,17 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\PlatsRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class PlatsController extends AbstractController
 {
     #[Route('/plats', name: 'app_plats')]
-    public function index(PlatsRepository $repository): Response
+    public function index(PlatsRepository $repository, PaginatorInterface $paginator, Request $request): Response
     {
+        $plats = $paginator->paginate(
+            $repository->findAll(),
+            $request->query->getInt('page', 1),
+            6 
+        );
+
         return $this->render('pages/plats/index.html.twig', [
-            'plats' => $repository->findAll()
+            'plats' => $plats
         ]);
     }
 }
