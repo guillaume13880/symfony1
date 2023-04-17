7699be79660fdca7da14eab37ab1edd8f3822a63
guillaume <guillaume.laurent13880@gmail.com>
2023-04-17 19:30:14 +0200

    mise en place form

--- src/Controller/PlatsController.php
+++ src/Controller/PlatsController.php
@@ -2,12 +2,15 @@
 
 namespace App\Controller;
 
+use App\Entity\Plats;
 use App\Repository\PlatsRepository;
+use App\Form\PlatsType;
 use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class PlatsController extends AbstractController
 {
@@ -24,11 +27,23 @@ class PlatsController extends AbstractController
         $plats = $paginator->paginate(
             $repository->findAll(),
             $request->query->getInt('page', 1),
-            6 
+            5 
         );
 
         return $this->render('pages/plats/index.html.twig', [
             'plats' => $plats
+        ]);
+    }
+
+
+    #[Route('/plats/nouveau', name: 'app_plats.new', methods: ['GET', 'POST'])]
+    public function new() : Response
+    {
+        $plats = new Plats();
+        $form = $this->createForm(PlatsType::class , $plats);
+
+        return $this->render('pages/plats/new.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
---
+++ src/Form/PlatsType.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Plats;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+class PlatsType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'minlength' => '2',
+                    'maxlength' => '50'
+                ],
+                'label' => 'Titre',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 50]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            ->add('image')
+            ->add('prix', MoneyType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                ],
+                'label' => 'Prix',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Positive(),
+                    new Assert\LessThan(200)
+                ]
+            ])
+            ->add('description', TextType::class, [
+                'attr' => [
+                    'class' => 'form-control',
+                    'min' => '2',
+                    'max' => '255'
+                ],
+                'label' => 'Description',
+                'label_attr' => [
+                    'class' => 'form-label mt-4'
+                ],
+                'constraints' => [
+                    new Assert\Length(['min' => 2, 'max' => 255]),
+                    new Assert\NotBlank()
+                ]
+            ])
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Plats::class,
+        ]);
+    }
+}
