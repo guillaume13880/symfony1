afa44bdea162e2814cdc729ffff892f259c92723
guillaume <guillaume.laurent13880@gmail.com>
2023-04-24 18:30:38 +0200

    mise en place de la modif du mot de passe ok

--- src/Controller/UserController.php
+++ src/Controller/UserController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\UserPasswordType;
 use App\Form\UserType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -58,6 +59,47 @@ class UserController extends AbstractController
 
         return $this->render('pages/user/edit.html.twig', [
             'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * Ce controller permet de modifier le mot de passe
+     */
+    #[Route('/utilisateur/edition-mot-de-passe/{id}', name: 'app_user.edit.password', methods: ['GET', 'POST'])]
+    public function editPassword(User $user, Request $request, EntityManagerInterface $manager, UserPasswordHasherInterface $hasher) : Response
+    {
+        $form = $this->createForm(UserPasswordType::class);
+
+        $form->handleRequest($request);
+        if($form->isSubmitted() && $form->isValid()) {
+            if($hasher->isPasswordValid($user, $form->getData()['plainPassword'])) {
+                $user->setPassword(
+                    $hasher->hashPassword(
+                        $user,
+                        $form->getData()['newPassword']
+                    )
+                );
+
+                $manager->persist($user);
+                $manager->flush();
+
+                $this->addFlash(
+                    'success',
+                    'Le mot de passe a été modifié avec succès'
+                );
+
+                return $this->redirectToRoute('home.index');
+
+            }else {
+                $this->addFlash(
+                    'warning',
+                    'Le mot de passe renseigné est incorrect '
+                );
+            }
+        }
+
+        return $this->render('pages/user/edit_password.html.twig', [
+            'form' => $form->createView()
         ]);
     }
 }
